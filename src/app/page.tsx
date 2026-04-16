cc74727ff3e33887dec1ab7c06b796829dcbd597
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2026-04-16 18:48:02 +0200

    fix title home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home",
   description:
     "Discover your next favorite film with curated selections and clean search tools in CineVault.",
   alternates: {
